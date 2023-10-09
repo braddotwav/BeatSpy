6d312154b69f079b113b49cf748d4b6371b5418d
RealJamako <bradto.45@gmail.com>
2023-10-09 20:16:38 +0100

    Cleaned up TrackInfo class

--- BeatSpy/Helpers/TrackInfo.cs
+++ BeatSpy/Helpers/TrackInfo.cs
@@ -5,41 +5,26 @@ namespace BeatSpy.Helpers;
 
 internal static class TrackInfo
 {
-    public static string GetDuration(float DurationMs)
-    {
-        var time = TimeSpan.FromMilliseconds(DurationMs);
-        return $"{time.Minutes}:{MathF.Round(time.Seconds)}";
-    }
+    public static string GetDuration(float durationMs) => TimeSpan.FromMilliseconds(durationMs).ToString("mm\\:ss");
 
-    public static string GetVolume(float Loudness)
-    {
-        var roundedLoudness = MathF.Round(Loudness, 2);
-        return $"{roundedLoudness}Db";
-    }
+    public static string GetVolume(float loudness) => $"{MathF.Round(loudness, 2)}Db";
 
-    public static string GetTempo(float Tempo)
-    {
-        var roundedTempo = MathF.Round(Tempo);
-        return $"{roundedTempo}BPM";
-    }
+    public static string GetTempo(float tempo) => $"{MathF.Round(tempo)}BPM";
 
-    public static string GetKey(int KeyIndex)
+    public static string GetKey(int pitchId) => (Pitches)pitchId switch
     {
-        return (Pitches)KeyIndex! switch
-        {
-            Pitches.PITCH_C => "C",
-            Pitches.PITCH_CSHARP => "C#/D♭",
-            Pitches.PITCH_D => "D",
-            Pitches.PITCH_DSHARP => "D#/E♭",
-            Pitches.PITCH_E => "E",
-            Pitches.PITCH_F => "F",
-            Pitches.PITCH_FSHARP => "F#/G♭",
-            Pitches.PITCH_G => "G",
-            Pitches.PITCH_GSHARP => "G#/A♭",
-            Pitches.PITCH_A => "A",
-            Pitches.PITCH_ASHARP => "A#/B♭",
-            Pitches.PITCH_B => "B",
-            _ => "Empty",
-        };
-    }
+        Pitches.PITCH_C => "C",
+        Pitches.PITCH_CSHARP => "C#/D♭",
+        Pitches.PITCH_D => "D",
+        Pitches.PITCH_DSHARP => "D#/E♭",
+        Pitches.PITCH_E => "E",
+        Pitches.PITCH_F => "F",
+        Pitches.PITCH_FSHARP => "F#/G♭",
+        Pitches.PITCH_G => "G",
+        Pitches.PITCH_GSHARP => "G#/A♭",
+        Pitches.PITCH_A => "A",
+        Pitches.PITCH_ASHARP => "A#/B♭",
+        Pitches.PITCH_B => "B",
+        _ => string.Empty,
+    };
 }
