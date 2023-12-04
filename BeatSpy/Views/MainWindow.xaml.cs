6fd47e652e9e6975371cb586ca30f8863816a14e
RealJamako <bradto.45@gmail.com>
2023-12-04 01:53:00 +0000

    Track title animation on mouse enter

--- BeatSpy/Views/MainWindow.xaml.cs
+++ BeatSpy/Views/MainWindow.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using BeatSpy.Helpers;
+using System.Windows.Media;
 using System.Windows.Input;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using BeatSpy.DataTypes.Constants;
-using System.Windows.Media.Animation;
-using System;
 
 namespace BeatSpy
 {
@@ -13,9 +14,13 @@ namespace BeatSpy
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly TitleAnimationBuilder titleAnimBuilder;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            titleAnimBuilder = new(TimeSpan.FromSeconds(5), 0.5);
         }
 
         /// <summary>
@@ -65,6 +70,36 @@ namespace BeatSpy
         private void OnBeatSpyClicked(object sender, RoutedEventArgs e)
         {
             BrowsUtil.OpenUrl(DefaultConstants.LINK_GITHUB_REPO);
+        }
+
+        /// <summary>
+        /// This method is fired when the user's mouse enters the title element
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnTrackTitleMouseEnter(object sender, MouseEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(async () =>
+            {
+                //Allow for a 500ms delay
+                await Task.Delay(500);
+
+                //Check if the mouse is hovered over the track title
+                if (TrackTitle.IsMouseOver)
+                {
+                    //Check if the tracks title width is passed the animation bounds
+                    if (titleAnimBuilder.ShouldAnimate(TrackTitle.ActualWidth) && !titleAnimBuilder.IsAnimationPlaying)
+                    {
+                        //Set the animation to location
+                        titleAnimBuilder.SetAnimationTo(-(TrackTitle.ActualWidth - 290));
+
+                        TranslateTransform translateTransform = new();
+                        TrackTitle.RenderTransform = translateTransform;
+
+                        titleAnimBuilder.StartAnimation(translateTransform);
+                    }
+                }
+            }));
         }
     }
 }
