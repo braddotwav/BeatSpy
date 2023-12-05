a480484ae046658de1cb9c1c450ed915d456f03c
RealJamako <bradto.45@gmail.com>
2023-12-05 21:43:58 +0000

    Added RemoveFocusCommand

---
+++ BeatSpy/Commands/RemoveFocusCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using BeatSpy.Commands.Base;
+
+namespace BeatSpy.Commands;
+
+internal class RemoveFocusCommand : CommandBase
+{
+    public override void Execute(object? parameter)
+    {
+        if(parameter is DependencyObject depObj)
+        {
+            //Clear the focus of focused object
+            FocusManager.SetFocusedElement(FocusManager.GetFocusScope(depObj), null);
+
+            //Clear keyboard focus
+            Keyboard.ClearFocus();
+        }
+    }
+}
