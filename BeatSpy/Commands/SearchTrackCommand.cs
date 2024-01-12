df53e88faff940595d6e916900b8b072cc4067cb
RealJamako <bradto.45@gmail.com>
2024-01-12 20:50:25 +0000

    Clean up search command

--- BeatSpy/Commands/SearchTrackCommand.cs
+++ BeatSpy/Commands/SearchTrackCommand.cs
@@ -1,48 +1,48 @@
 ﻿using System;
 using SpotifyAPI.Web;
 using BeatSpy.Models;
+using BeatSpy.Helpers;
 using BeatSpy.Services;
 using BeatSpy.ViewModels;
 using BeatSpy.Commands.Base;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using BeatSpy.DataTypes.Interfaces;
 
 namespace BeatSpy.Commands;
 
 internal class SearchTrackCommand : AsyncCommandBase
 {
-    private readonly IApplicationCommands applicationCommands;
     private readonly IMessageDisplayService messageDisplayService;
     private readonly ISpotifyService spotifyService;
     private readonly TrackViewModel trackViewModel;
 
-    private string currentQuery = string.Empty;
+    private string currentSearched = string.Empty;
+    private string searchQuery = string.Empty;
 
-    public SearchTrackCommand(ISpotifyService spotifyService, IApplicationCommands applicationCommands, TrackViewModel trackViewModel, IMessageDisplayService messageDisplayService)
+    public SearchTrackCommand(ISpotifyService spotifyService, TrackViewModel trackViewModel, IMessageDisplayService messageDisplayService)
     {
         this.messageDisplayService = messageDisplayService;
-        this.applicationCommands = applicationCommands;
         this.spotifyService = spotifyService;
         this.trackViewModel = trackViewModel;
     }
 
     public override bool CanExecute(object? parameter)
     {
-        string queryContent = ((TextBox)parameter!).Text;
-        return spotifyService.IsLoggedIn && !string.IsNullOrEmpty(((TextBox)parameter!).Text) && !string.Equals(currentQuery, queryContent);
+        searchQuery = ((TextBox)parameter!).Text;
+
+        return spotifyService.IsLoggedIn && !string.IsNullOrEmpty(searchQuery) && !string.Equals(searchQuery, currentSearched);
     }
 
     protected override async Task ExcuteAsync(object? parameter)
     {
         try
         {
-            FullTrack track = await spotifyService.GetTrack(((TextBox)parameter!).Text);
+            currentSearched = searchQuery;
+            FullTrack track = await spotifyService.GetTrack(searchQuery);
             TrackAudioFeatures features = await spotifyService.GetAudioTrackFeatures(track.Id);
 
             trackViewModel.SetCurrentTrack(new BeatTrack(track, features));
-            currentQuery = ((TextBox)parameter!).Text;
-            applicationCommands.RemoveFocus.Execute(parameter);
+            ApplicationHelper.RemoveElementFocus(parameter);
         }
         catch (Exception ex)
         {
