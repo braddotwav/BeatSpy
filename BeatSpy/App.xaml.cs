08aad8f345ec33dcbd17de27162d93b5d3d38816
braddotwav <bradto.45@gmail.com>
2024-09-15 20:50:22 +0100

    Disallow multiple instances to be opened

--- BeatSpy/App.xaml.cs
+++ BeatSpy/App.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Windows;
+using System.Threading;
 using BeatSpy.Services;
 using BeatSpy.ViewModels;
+using System.Diagnostics;
 using BeatSpy.ViewModels.Base;
 using BeatSpy.DataTypes.Enums;
+using System.Runtime.InteropServices;
 
 namespace BeatSpy;
 
@@ -12,11 +15,38 @@ namespace BeatSpy;
 /// </summary>
 public partial class App : Application
 {
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
     private ViewModelBase? mainViewModel;
     private readonly ISpotifyAuthenticationService spotifyAuth;
 
+    private readonly Mutex mutex;
+
     public App()
     {
+        mutex = new Mutex(true, "BeatSpy", out bool isNewInstance);
+
+        if (!isNewInstance)
+        {
+            Process current = Process.GetCurrentProcess();
+            foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+            {
+                if (process.Id != current.Id)
+                {
+                    ShowWindow(process.MainWindowHandle, 9);
+                    SetForegroundWindow(process.MainWindowHandle);
+                    Current.Shutdown();
+                    break;
+                }
+            }
+        }
+
         spotifyAuth = new SpotifyAuthenticationService();
     }
 
@@ -49,6 +79,8 @@ public partial class App : Application
 
     protected override void OnExit(ExitEventArgs e)
     {
+        mutex.ReleaseMutex();
+        mutex.Dispose();
         mainViewModel!.Dispose();
         spotifyAuth.Dispose();
         base.OnExit(e);
