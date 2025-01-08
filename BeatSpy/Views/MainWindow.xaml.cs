e5f083327baa7166ee114bf03c8c87eb3d4a0b96
Brad <90891134+braddotwav@users.noreply.github.com>
2025-01-08 12:52:06 +0000

    Completed marquee text control

--- BeatSpy/Views/MainWindow.xaml.cs
+++ BeatSpy/Views/MainWindow.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Windows;
-using BeatSpy.Services;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Threading.Tasks;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace BeatSpy
 {
@@ -13,17 +9,9 @@ namespace BeatSpy
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly TitleAnimationService titleAnimationService;
-        private readonly TranslateTransform titleTransform;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            titleAnimationService = new TitleAnimationService();
-
-            titleTransform = new TranslateTransform();
-            TrackTitle.RenderTransform = titleTransform;
         }
 
         /// <summary>
@@ -63,57 +51,6 @@ namespace BeatSpy
             {
                 contextMenu.DataContext = DataContext;
             }
-        }
-
-        /// <summary>
-        /// This method is fired when the user's mouse enters the title element
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnTrackTitleMouseEnter(object sender, MouseEventArgs e)
-        {
-            // If the title animation is already playing return
-            if (titleAnimationService.IsPlaying) return;
-
-            Dispatcher.BeginInvoke(new Action(async () =>
-            {
-                // Wait a second until procceeding
-                await Task.Delay(1000);
-
-                // Insure the user is still hoovering over the title and that the title is out of bounds
-                if (TrackTitle.IsMouseOver && titleAnimationService.IsTitleOutOfView(TrackTitle.ActualWidth))
-                {
-                    // Start the title animation
-                    titleAnimationService.PlayAnimation(titleTransform, TrackTitle.ActualWidth);
-                }
-            }));
-        }
-
-        /// <summary>
-        /// This method is fired when the track title is updated
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnTrackTitleUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
-        {
-            // Reset the track title if the animation is playing
-            if (titleAnimationService.IsPlaying)
-            {
-                titleAnimationService.StopAnimation(titleTransform);
-            }
-
-            Dispatcher.BeginInvoke(new Action(async () =>
-            {
-                // Wait a second until procceeding
-                await Task.Delay(1000);
-
-                // Insure that the title is out of bounds
-                if (titleAnimationService.IsTitleOutOfView(TrackTitle.ActualWidth))
-                {
-                    // Start the title animation
-                    titleAnimationService.PlayAnimation(titleTransform, TrackTitle.ActualWidth);
-                }
-            }));
         }
     }
 }
