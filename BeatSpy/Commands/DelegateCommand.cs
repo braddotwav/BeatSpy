aef1b964c246c6e48bdb99c7cc0b81a3623d2993
Brad <90891134+braddotwav@users.noreply.github.com>
2025-03-03 05:54:19 +0000

    Create DelegateCommand.cs

---
+++ BeatSpy/Commands/DelegateCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BeatSpy.Commands;
+
+internal class DelegateCommand(Action<object> execute, Func<object, bool> canExecute) : CommandBase
+{
+    private readonly Action<object> _execute = execute;
+    private readonly Func<object, bool> _canExecute = canExecute;
+
+    public override void Execute(object? parameter)
+    {
+        _execute?.Invoke(parameter ?? EventArgs.Empty);
+    }
+
+    public override bool CanExecute(object? parameter)
+    {
+        return _canExecute(parameter ?? EventArgs.Empty);
+    }
+}
