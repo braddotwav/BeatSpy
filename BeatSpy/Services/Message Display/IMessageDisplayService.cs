58f3987257a0dab5ef31ab22dbcb883309f2316f
Brad <90891134+braddotwav@users.noreply.github.com>
2025-03-03 23:26:36 +0000

    Cleaned up the display service

--- BeatSpy/Services/Message Display/IMessageDisplayService.cs
+++ BeatSpy/Services/Message Display/IMessageDisplayService.cs
@@ -4,10 +4,23 @@ namespace BeatSpy.Services;
 
 public interface IMessageDisplayService
 {
-    public string Message { get; }
-    public bool IsMessageEmpty { get; }
-    public event Action OnMessageUpdated;
+    public event Action<string> OnMessageReceived;
+
+    /// <summary>
+    /// Displays a message with the option of being silent.
+    /// </summary>
+    /// <param name="message">Message to log</param>
+    /// <param name="silent">Should the message be shown to the user</param>
     public void DisplayInfoMessage(string message, bool silent = false);
+
+    /// <summary>
+    /// Displays and logs an exception message
+    /// </summary>
+    /// <param name="ex"></param>
     public void DisplayErrorMessage(Exception ex);
+
+    /// <summary>
+    /// Clears the message to a empty string
+    /// </summary>
     public void ClearMessage();
 }
--- BeatSpy/Services/Message Display/MessageDisplayService.cs
+++ BeatSpy/Services/Message Display/MessageDisplayService.cs
@@ -1,5 +1,6 @@
 ﻿using NLog;
 using System;
+using System.Diagnostics;
 
 namespace BeatSpy.Services;
 
@@ -7,48 +8,28 @@ internal sealed class MessageDisplayService : IMessageDisplayService
 {
     private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
-    private string message = string.Empty;
+    public event Action<string>? OnMessageReceived;
 
-    public string Message
-    {
-        get { return message; }
-        set
-        {
-            message = value;
-            OnMessageUpdated?.Invoke();
-        }
-    }
-
-    public bool IsMessageEmpty => string.IsNullOrEmpty(message);
-
-    public event Action? OnMessageUpdated;
-
-    /// <summary>
-    /// Clears the message to a empty string
-    /// </summary>
     public void ClearMessage()
     {
-        Message = string.Empty;
+        OnMessageReceived?.Invoke(string.Empty);
     }
 
-    /// <summary>
-    /// Displays an exception message and logs the exception.
-    /// </summary>
-    /// <param name="ex"></param>
     public void DisplayErrorMessage(Exception ex)
     {
-        Message = ex.Message;
+        OnMessageReceived?.Invoke(ex.Message);
         logger.Error(ex);
     }
 
-    /// <summary>
-    /// Displays an info message to the UI with the option of being silent.
-    /// </summary>
-    /// <param name="message">Message to log</param>
-    /// <param name="silent">Should the message be shown to the user</param>
-    public void DisplayInfoMessage(string message, bool silent)
+    public void DisplayInfoMessage(string message, bool silent = false)
     {
-        Message = silent ? string.Empty : message;
-        logger.Info(message);
+        if (silent)
+        {
+            logger.Info(message);
+            return;
+        }
+
+        OnMessageReceived?.Invoke(message);
+        Debug.WriteLine(message);
     }
 }
