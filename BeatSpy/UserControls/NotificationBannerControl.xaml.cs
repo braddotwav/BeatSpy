166e7b69040e0b7b53884432b4bea1663e25f96d
Brad <90891134+braddotwav@users.noreply.github.com>
2025-03-25 05:04:47 +0000

    NotificationBanner > NotificationBannerControl

---
+++ BeatSpy/UserControls/NotificationBannerControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+
+namespace BeatSpy.UserControls
+{
+    /// <summary>
+    /// Interaction logic for NotificationBanner.xaml
+    /// </summary>
+    public partial class NotificationBannerControl : UserControl
+    {
+        public NotificationBannerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
