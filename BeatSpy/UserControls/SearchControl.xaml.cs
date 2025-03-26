6a656fac2cd23e29114f02da061eba0f8c9c4e1b
Brad <90891134+braddotwav@users.noreply.github.com>
2025-03-26 07:00:14 +0000

    Randomize the placeholder text in the search

--- BeatSpy/UserControls/SearchControl.xaml.cs
+++ BeatSpy/UserControls/SearchControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using BeatSpy.Helpers;
 using System.Windows.Input;
 using System.Windows.Controls;
 
@@ -9,6 +10,15 @@ namespace BeatSpy.UserControls
     /// </summary>
     public partial class SearchControl : UserControl
     {
+        private readonly string[] placeholders = 
+            ["Search for a song…", 
+            "Type a song title to analyze…", 
+            "What song are you curious about?",
+            "Get insights on any track…",
+            "Search by song, artist, or album…"];
+
+        private Label? placeholderLabel;
+
         public ICommand SearchCommand
         {
             get { return (ICommand)GetValue(SearchCommandProperty); }
@@ -18,7 +28,6 @@ namespace BeatSpy.UserControls
         // Using a DependencyProperty as the backing store for SearchCommand.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SearchCommandProperty =
             DependencyProperty.Register("SearchCommand", typeof(ICommand), typeof(SearchControl), new PropertyMetadata(null));
-
 
         public SearchControl()
         {
@@ -32,15 +41,26 @@ namespace BeatSpy.UserControls
         /// <param name="e"></param>
         private void OnSearchQueryPreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key is Key.Enter)
-            {
-                if (SearchCommand == null || !SearchCommand.CanExecute(Search.Text)) return;
+            if (e.Key is not Key.Enter) return;
+            if (SearchCommand == null || !SearchCommand.CanExecute(Search.Text)) return;
+            
+            SetRandomPlaceholderContent();
+            SearchCommand.Execute(Search.Text);
+            Search.ScrollToHome();
+            FocusManager.SetFocusedElement(FocusManager.GetFocusScope(Search), null);
+            Keyboard.ClearFocus();
+        }
 
-                SearchCommand.Execute(Search.Text);
-                Search.ScrollToHome();
-                FocusManager.SetFocusedElement(FocusManager.GetFocusScope(Search), null);
-                Keyboard.ClearFocus();
-            }
+        private void SetRandomPlaceholderContent()
+        {
+            if (placeholderLabel is null) return;
+            placeholderLabel.Content = placeholders[RandomHelper.Range(0, placeholders.Length)];
+        }
+
+        private void OnUserControlLoaded(object sender, RoutedEventArgs e)
+        {
+            placeholderLabel = (Label)Search.Template.FindName("SearchPlaceholder", Search);
+            SetRandomPlaceholderContent();
         }
     }
 }
